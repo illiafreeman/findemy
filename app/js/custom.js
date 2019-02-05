471c0284ccfacdf3c9654ec356fc6af106ceae8d
freeman <iviewn@gmail.com>
2019-02-05 23:01:55 +0300

    work

--- app/js/custom.js
+++ app/js/custom.js
@@ -340,9 +340,9 @@ owl = $('.home-slider').on('initialized.owl.carousel', function(e) {
     dots: false,
     autoplayTimeout: 5000,
     mouseDrag: false,
-    //touchDrag: false,
-    //pullDrag: false,
-    //autoplay: true
+    touchDrag: false,
+    pullDrag: false,
+    autoplay: true
 });
 
 $('#circ, .home-slider__preview').click(function() {
@@ -358,7 +358,7 @@ $('.home-slider').on('changed.owl.carousel', function(e) {
     var src = $(e.target).find(".owl-item").eq(current).find(".home-slider__preview").append('<div id="circ"></div>');
 
     owl.trigger('stop.owl.autoplay');
-    //owl.trigger('play.owl.autoplay');
+    owl.trigger('play.owl.autoplay');
 
     var bar = new ProgressBar.Circle(circ, {
         color: '#aaa',
